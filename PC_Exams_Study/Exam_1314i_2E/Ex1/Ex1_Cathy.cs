0a60664c772586fc2b7153e9259dac08e1f2e1cf
Cátia Ormonde <a36923@alunos.isel.pt>
2014-06-23 19:09:08 +0100

    1314i - 2ºexame: Ex.2 Blacboard actualizado e Ex.1 feito

--- PC_Exams_Study/Exam_1314i_2E/Ex1/Ex1_Cathy.cs
+++ PC_Exams_Study/Exam_1314i_2E/Ex1/Ex1_Cathy.cs
@@ -6,111 +6,82 @@ namespace Exam_1314i_2E.Ex1
     public class Ex1_Cathy
     {
         /*
-         * [4] Usando monitores implícitos do Java ou da CLI, implemente o sincronizador blackboard, que viabiliza a divulgação de mensagens com prazo de validade. A interface pública do sincronizador é apresentada de seguida em C#.
-            public class BlackBoard<T> where T : class {
-                public void Write(T message, int duration);
-                public T Read(int timeout);
-                public void Clear();
+         * [3,5] Considere a classe UnsafeSpinReadWriteLock, não thread-safe, apresentada a seguir em C#:
+            public class UnsafeSpinReadWriteLock {
+                
+         *      private int state; // 0 means free, -1 means writing, and > 0 means reading
+                
+         *      public void LockRead() {
+                    SpinWait sw = new SpinWait();
+                    while (state < 0)
+                    sw.SpinOnce();
+                    state++;
+                }
+                public void LockWrite() {
+                    SpinWait sw = new SpinWait();
+                    while (state != 0)
+                    sw.SpinOnce();
+                    state = -1;
+                }
+                public void UnlockRead() { state--; }
+         * 
+                public void UnlockWrite() { state = 0; }
             }
-            O sincronizador blackboard suporta a afixação de mensagens a serem lidas por várias threads. 
-         * 
-         * A operação Write afixa a mensagem recebida (message), que permanecerá válida durante o intervalo de tempo duration, 
-         * especificado em milissegundos. No caso de ainda existir uma mensagem válida no blackboard, a nova mensagem substitui a existente.
-         * 
-            A operação Read promove a leitura da mensagem afixada no blackboard, bloqueando a thread invocante caso não exista nenhuma mensagem válida. 
-         * A operação termina: com sucesso, retornando a última mensagem válida; ou com insucesso, lançando a respectiva excepção, 
-         * caso o tempo máximo de espera (timeout) seja excedido ou o bloqueio d thread seja cancelado.
-         * 
-            A operação Clear remove, caso exista, a mensagem afixada no blackboard.
+            Indique as razões pelas quais a implementação não é thread-safe e, 
+         * sem recorrer à utilização de primitivas de sincronização bloqueantes, 
+         * apresente as alterações necessárias (em Java ou C#) para a tornar thread-safe
          */
 
-        public class BlackBoard<T> where T : class
+
+        // Resposta:
+
+        // A implementação apresentada não é thread safe pois não há garantia, tendo em conta que o código é usado por várias threads,
+        // de que o estado da classe lido é o que foi escrito pela última vez (state não é "volatile").
+
+        //      - Volatile -> É usado para impedir que o compilador faça optimizações, "ignorando" algum código. 
+        //                     Ao usar volatile: lê-se sempre o último valor que foi escrito na variável.
+
+        // Além disso, não estão a ser usadas instruções atómicas sobre "state". Isto é necessário pois as instruções atómicas são feitas
+        // usando apenas uma operação. Por exemplo, o "atomic increment", para carregar uma variável, incrementá-la e guardar novamente o valor, usa só 1 operação.
+        // Assim, ao usar instruções atómicas, temos a garantia de que "nada" se mete no meio da execução da operação desejada.
+
+        // Apresentação de uma solução Thread-Safe:
+
+        #pragma warning disable 420 // para impedir o aparecimento de warnings sempre que se usar o nome de uma var. volatile c/ prefixo ref
+        public class SafeSpinReadWriteLock
         {
-            class Message<T>
-            {
-                public T message { get; set; }
-                public int timeMessageWasWritten { get; set; }
-                public int messageDuration { get; set; }
+            private volatile int state; // 0 means free, -1 means writing, and > 0 means reading
 
-                public Message(T msg, int beginTime, int durationTime)
-                {
-                    message = msg;
-                    timeMessageWasWritten = beginTime;
-                    messageDuration = durationTime;
-                } 
+            public void LockRead()
+            {
+                SpinWait sw = new SpinWait();
+                
+                while (state < 0)
+                    sw.SpinOnce();
+
+                Interlocked.Increment(ref state);
             }
 
-            private Message<T> _msg; 
+            public void LockWrite() {
+                
+                SpinWait sw = new SpinWait();
+                
+                while (state != 0)
+                    sw.SpinOnce();
 
-            public BlackBoard()
-            {
-                _msg = null;
+                int readState = state;
+                Interlocked.CompareExchange(ref state, -1, readState);
             }
 
-            public void Write(T message, int duration)
+            public void UnlockRead()
             {
-                if (duration == 0)
-                    return;
-
-                lock (this)
-                {
-                    // mesmo que ainda exista uma mensagem válida, a nova msg substitui a existente
-                     _msg = new Message<T>(message, Environment.TickCount, duration);
-                    Monitor.PulseAll(this);
-                }
+                Interlocked.Increment(ref state);
             }
 
-            public T Read(int timeout)
+            public void UnlockWrite()
             {
-                if (timeout == 0)
-                    return null;
-
-                lock (this)
-                {
-                    if (_msg != null) // Ver se existe mensagem
-                    {
-                        // Se o tempo de validade da mensagem ainda nao passou... Retornar a mensagem.
-                        if (_msg.messageDuration >= Environment.TickCount - _msg.timeMessageWasWritten)
-                        {
-                            return _msg.message;
-                        }
-                        Clear(); // Se o tempo de validade ja passou, limpar
-                    }
-
-                    // Se nao existe mensagem (ou se o tempo de validade dela ja tinha passado)... Esperar...
-
-                    int initialTime = Environment.TickCount;
-                    do
-                    {
-                        try
-                        {
-                            Monitor.Wait(this, timeout);
-                        }
-                        catch (ThreadInterruptedException iex)
-                        {
-                            throw; //rethrow, right? 
-                        }
-                        if (_msg != null)
-                        {
-                            if (_msg.messageDuration < Environment.TickCount - _msg.timeMessageWasWritten)
-                                Clear();
-                            else
-                                return _msg.message;
-                        }
-                        if (SyncUtils.AdjustTimeout(ref initialTime, ref timeout)<=0)
-                        {
-                            throw new TimeoutException();
-                        }
-                    } while (true);
-                }
-            }
-
-            public void Clear()
-            {
-                lock (this)
-                {
-                    _msg = null;
-                }
+                int readState = state;
+                Interlocked.CompareExchange(ref state, 0, readState);
             }
         }
     }
--- PC_Exams_Study/Exam_1314i_2E/Ex2/Ex2_Cathy.cs
+++ PC_Exams_Study/Exam_1314i_2E/Ex2/Ex2_Cathy.cs
@@ -9,69 +9,111 @@ namespace Exam_1314i_2E.Ex2
 {
     class Ex2_Cathy
     {
+        /*
+                 * [4] Usando monitores implícitos do Java ou da CLI, implemente o sincronizador blackboard, que viabiliza a divulgação de mensagens com prazo de validade. A interface pública do sincronizador é apresentada de seguida em C#.
+                    public class BlackBoard<T> where T : class {
+                        public void Write(T message, int duration);
+                        public T Read(int timeout);
+                        public void Clear();
+                    }
+                    O sincronizador blackboard suporta a afixação de mensagens a serem lidas por várias threads. 
+                 * 
+                 * A operação Write afixa a mensagem recebida (message), que permanecerá válida durante o intervalo de tempo duration, 
+                 * especificado em milissegundos. No caso de ainda existir uma mensagem válida no blackboard, a nova mensagem substitui a existente.
+                 * 
+                    A operação Read promove a leitura da mensagem afixada no blackboard, bloqueando a thread invocante caso não exista nenhuma mensagem válida. 
+                 * A operação termina: com sucesso, retornando a última mensagem válida; ou com insucesso, lançando a respectiva excepção, 
+                 * caso o tempo máximo de espera (timeout) seja excedido ou o bloqueio d thread seja cancelado.
+                 * 
+                    A operação Clear remove, caso exista, a mensagem afixada no blackboard.
+                 */
 
-        class Blackboard<T> where T : class
+        public class BlackBoard<T> where T : class
         {
-            private T msg;
-            
-            public Blackboard(T m)
+            class Message<T>
             {
-                msg = m;
-            } 
+                public T message { get; set; }
+                public int timeMessageWasWritten { get; set; }
+                public int messageDuration { get; set; }
 
-            // Afixa a mensagem recebida, que permanecerá válida durante o tempo especificado (duration)
+                public Message(T msg, int beginTime, int durationTime)
+                {
+                    message = msg;
+                    timeMessageWasWritten = beginTime;
+                    messageDuration = durationTime;
+                }
+            }
+
+            private Message<T> _msg;
+
+            public BlackBoard()
+            {
+                _msg = null;
+            }
+
             public void Write(T message, int duration)
             {
-                if (message == null)
+                if (duration == 0)
                     return;
-                lock (msg)
-                {
-                    int lastTime = duration != Timeout.Infinite ? Environment.TickCount: 0;
-                    msg = message;
-                    Monitor.PulseAll(msg);
-                    do
-                    {
-                        Monitor.Wait(msg, duration);
 
-                    } while (SyncUtils.AdjustTimeout(ref lastTime, ref duration)>0);
+                lock (this)
+                {
+                    // mesmo que ainda exista uma mensagem válida, a nova msg substitui a existente
+                    _msg = new Message<T>(message, Environment.TickCount, duration);
+                    Monitor.PulseAll(this);
                 }
-                Clear();
             }
 
-            // bloqueia a thread invocante caso nao exista nenhuma mensagem
             public T Read(int timeout)
             {
-                if(timeout==0)
+                if (timeout == 0)
                     return null;
-                
-                T msgToRead = null;
-                int lastTime = timeout != Timeout.Infinite ? Environment.TickCount : 0;
-                do
+
+                lock (this)
                 {
-                    if ((msgToRead = msg) != null)
-                        break;
-                    try
+                    if (_msg != null) // Ver se existe mensagem
                     {
-                        Monitor.Wait(msg);
+                        // Se o tempo de validade da mensagem ainda nao passou... Retornar a mensagem.
+                        if (_msg.messageDuration >= Environment.TickCount - _msg.timeMessageWasWritten)
+                        {
+                            return _msg.message;
+                        }
+                        Clear(); // Se o tempo de validade ja passou, limpar
                     }
-                    catch (ThreadInterruptedException iex)
+
+                    // Se nao existe mensagem (ou se o tempo de validade dela ja tinha passado)... Esperar...
+
+                    int initialTime = Environment.TickCount;
+                    do
                     {
-                        return null;
-                    }
-                    if (SyncUtils.AdjustTimeout(ref lastTime, ref timeout) <= 0)
-                    {
-                        throw new TimeoutException();
-                    }
-                } while (true);
-                return msgToRead;
+                        try
+                        {
+                            Monitor.Wait(this, timeout);
+                        }
+                        catch (ThreadInterruptedException iex)
+                        {
+                            throw; //rethrow, right? 
+                        }
+                        if (_msg != null)
+                        {
+                            if (_msg.messageDuration < Environment.TickCount - _msg.timeMessageWasWritten)
+                                Clear();
+                            else
+                                return _msg.message;
+                        }
+                        if (SyncUtils.AdjustTimeout(ref initialTime, ref timeout) <= 0)
+                        {
+                            throw new TimeoutException();
+                        }
+                    } while (true);
+                }
             }
 
-            // remove, caso exista, a mensagem afixada
             public void Clear()
             {
-                lock (msg)
+                lock (this)
                 {
-                    msg = null;
+                    _msg = null;
                 }
             }
         }
