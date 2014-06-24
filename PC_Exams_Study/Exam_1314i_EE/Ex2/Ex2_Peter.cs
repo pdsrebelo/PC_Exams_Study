e586cca63ff4e8670c74599cb47dba64975ddd4b
Pedro Rebelo <p3dro_rebelo@hotmail.com>
2014-06-24 23:44:23 +0100

    I haz the future!

--- PC_Exams_Study/Exam_1314i_EE/Ex2/Ex2_Peter.cs
+++ PC_Exams_Study/Exam_1314i_EE/Ex2/Ex2_Peter.cs
@@ -2,11 +2,74 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Exam_1314i_2E;
 
 namespace Exam_1314i_EE.Ex2
 {
     class Ex2_Peter
     {
+        /// <summary>
+        /// Usando monitores Java ou CLI, implemente o sincronizador future holder usado para hospedar dados 
+        /// resultantes de cálculos demorados. A interface pública do sincronizador é apresentada de seguida.
+        /// A operação GetValue bloqueia as threads evocantes até que os dados sejam disponibilizados através da 
+        /// chamada a SetValue. Como as instâncias desta classe são de utilização única, chamadas subsequentes a 
+        /// SetValue produzem excepção (InvalidOperationException). A operação GetValue retorna os dados 
+        /// hospedados, ou null caso ocorra timeout. O sincronizador suporta cancelamento das thread sem espera.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public class FutureHolder<T>
+        {
+            private T _value;
+
+            public void SetValue(T value)
+            {
+                lock (this)
+                {
+                    if(IsValueAvailable())
+                        throw new InvalidOperationException("STAHP CALLING DIS WTF!!11");
+
+                    _value = value;
+
+                    Monitor.Pulse(this);
+                }
+            }
+
+            public T GetValue(int timeout)
+            {
+                lock (this)
+                {
+                    if (IsValueAvailable())
+                        return _value;
+
+                    int lastTime = Environment.TickCount;
+
+                    do
+                    {
+                        Monitor.Wait(this, timeout);
+
+                        if (IsValueAvailable())
+                            return _value;
+
+                        if (SyncUtils.AdjustTimeout(ref lastTime, ref timeout) == 0)
+                        {
+                            return default(T);
+                        }
+
+                    } while (true);
+                }
+            }
+
+            public bool IsValueAvailable()
+            {
+                lock (this)
+                {
+                    if (_value != null) // TODO: Not sure about this one :/
+                        return true;
+                    return false;
+                }
+            }
+        }
     }
 }
--- PC_Exams_Study/Exam_1314i_EE/Ex5/Ex5_Peter.cs
+++ PC_Exams_Study/Exam_1314i_EE/Ex5/Ex5_Peter.cs
@@ -8,5 +8,16 @@ namespace Exam_1314i_EE.Ex5
 {
     class Ex5_Peter
     {
+        public static int Execute(Func<int> a, Func<int> b, Func<int> c, Func<int,int> d)
+        {
+        return d(a() + b() + c());
+        }
+        
+        /* 
+            Produza uma implementação da função que, fazendo uso da TPL, tire partido da possível 
+            existência de múltiplos processadores.
+        */
+
+
     }
 }
